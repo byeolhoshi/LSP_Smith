132893c433d77287f3850f3b1ff5f656fd76f6d3
ACL <astersmith019@gmail.com>
2022-04-22 09:12:17 -0500

    finishing code

--- Assignment5/src/org/howard/edu/lsp/finalExam/MapUtilities.java
+++ Assignment5/src/org/howard/edu/lsp/finalExam/MapUtilities.java
@@ -4,6 +4,22 @@ import java.util.Map;
 
 public class MapUtilities {
 	public static int commonKeyValuePairs(HashMap<String, String> map1, HashMap<String,String> map2) throws NullMapException {
+		int count = 0; 
+		if (map1 == null || map2 == null ) { 
+			throw new NullMapException(); 
+		}
 		
+		for(Map.Entry<String, String> element : map1.entrySet()) {
+			String k = element.getKey(); 
+			String v = element.getValue(); 
+			
+			if(map2.containsKey(k)) {
+				String v2 = map2.get(k); 
+				if(v == v2) {
+					count++; 
+				}
+			}
+		}
+		return count; 
 	}
 }
